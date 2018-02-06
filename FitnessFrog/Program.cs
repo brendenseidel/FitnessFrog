ec3d80dcadc7ffda527feb9b63a90d050d165bf5
Brenden Seidel <brenseid@gmail.com>
2018-02-06 09:13:19 -0500

    added try catch for format exception

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -6,7 +6,61 @@ namespace FitnessFrog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.Write("Enter how many minutes you excercised or type \"quit\" to exit: ");
+
+                string entry = Console.ReadLine();
+
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    try
+                    {
+                        int minutes = int.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                            continue;
+                        }
+                        if (minutes < 10)
+                        {
+                            Console.WriteLine("Better than nothing, amirite?");
+                        }
+                        else if (minutes < 30)
+                        {
+                            Console.WriteLine("Now you're cooking.");
+                        }
+                        else if (minutes < 60)
+                        {
+                            Console.WriteLine("Dang, yo crazy man");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay just stop, okay.");
+                        }
+
+                        runningTotal += minutes;
+
+                        Console.WriteLine("You've entered " + runningTotal + " minutes.");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That is not valid input");
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Goodbye");
         }
     }
 }
